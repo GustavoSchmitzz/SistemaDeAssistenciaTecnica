c39d880a5243da6d94d137b490934c931b59d1e1
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-07-20 23:15:03 -0400

    feat: Cria a entidade StatusServico.

---
+++ src/main/java/com/assistencia/entity/StatusServico.java
@@ -0,0 +1,11 @@
+package com.assistencia.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StatusServico {
+    private Integer id;
+    private String Status;
+}
