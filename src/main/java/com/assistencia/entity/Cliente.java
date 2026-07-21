e77d5f3a621404d5940eea5201295550742e8f41
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-07-20 21:57:46 -0400

    feat: Cria a entridade Cliente.

---
+++ src/main/java/com/assistencia/entity/Cliente.java
@@ -0,0 +1,13 @@
+package com.assistencia.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+    private Integer id;
+    private String nome;
+    private String email;
+    private String telefone;
+}
