7206bfc73e1c591c4b4df63074be89830c0ad248
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-07-20 22:13:23 -0400

    feat: Cria a entidade Tecnico.

---
+++ src/main/java/com/assistencia/entity/Tecnico.java
@@ -0,0 +1,13 @@
+package com.assistencia.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Tecnico {
+    private Integer id;
+    private String nome;
+    private String telefone;
+    private String especialidade;
+}
