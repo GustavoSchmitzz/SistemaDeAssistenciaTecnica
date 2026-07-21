cbb03a9684df945a8e854a6df0c61a4afa926931
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-07-20 22:04:05 -0400

    feat: cria a entidade Fornecedor.

---
+++ src/main/java/com/assistencia/entity/Fornecedor.java
@@ -0,0 +1,14 @@
+package com.assistencia.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Fornecedor {
+    private Integer id;
+    private String nome;
+    private String telefone;
+}
+
+
