4cda84b8171191dce0fe1bc2b11c53289e348281
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-07-27 12:38:15 -0400

    refactor: refatora o metodo ClienteService.

--- src/main/java/com/assistencia/service/ClienteService.java
+++ src/main/java/com/assistencia/service/ClienteService.java
@@ -14,22 +14,21 @@ public class ClienteService {
         if (cliente == null) {
             throw new IllegalArgumentException("cliente nao pode ser nulo.");
         }
-        if(cliente.getNome() == null || cliente.getNome().trim().isEmpty() ||
-                cliente.getNome().trim().length() > 100) {
+        if(cliente.getNome() == null || cliente.getNome().trim().isEmpty()) {
             throw new IllegalArgumentException("nome nao pode ser nulo, vazio ou ter mais de 100 caracteres.");
         }
-        if(cliente.getEmail() == null || cliente.getEmail().trim().isEmpty() ||
-                cliente.getEmail().trim().length() > 100) {
+        if(cliente.getEmail() == null || !cliente.getEmail()
+                .matches("^[a-zA-Z0-9À-ÿ._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
             throw new IllegalArgumentException("email nao pode ser nulo, vazio ou ter mais de 100 caracteres.");
         }
-        if(cliente.getTelefone() == null || cliente.getTelefone().trim().isEmpty() ||
-                cliente.getTelefone().trim().length() > 11) {
+        if(cliente.getTelefone() == null ||
+                !cliente.getTelefone().trim().matches("^[0-9]{10,11}$")) {
             throw new IllegalArgumentException("telefone nao pode ser nulo, vazio ou ter mais de 11 caracteres.");
         }
 
-        cliente.setNome(cliente.getNome().trim());
-        cliente.setEmail(cliente.getEmail().trim());
-        cliente.setTelefone(cliente.getTelefone().trim());
+        cliente.setNome(cliente.getNome().trim().toLowerCase());
+        cliente.setEmail(cliente.getEmail().trim().toLowerCase());
+        cliente.setTelefone(cliente.getTelefone().trim().toLowerCase());
 
         return clienteRepository.cria(cliente);
     }
