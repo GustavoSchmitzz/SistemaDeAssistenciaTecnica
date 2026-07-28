9190aa070e97bddab726e00c4429f7a2ac265a4c
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-07-27 22:36:39 -0400

    feat: cria o metodo buscaPorId em StatusServicoService.

---
+++ src/main/java/com/assistencia/service/StatusServicoService.java
@@ -0,0 +1,23 @@
+package com.assistencia.service;
+
+import com.assistencia.entity.StatusServico;
+import com.assistencia.repository.StatusServicoRepository;
+
+public class StatusServicoService {
+    private final StatusServicoRepository statusServicoRepository;
+    public StatusServicoService(StatusServicoRepository statusServicoRepository) {
+        this.statusServicoRepository = statusServicoRepository;
+    }
+
+    public StatusServico buscaPorId(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id nao pode ser igual ou menor que zero.");
+        }
+        StatusServico status = statusServicoRepository.buscaOID(id);
+        if (status == null) {
+            throw new RuntimeException("Status nao encontrado no banco de dados.");
+        }
+
+        return status;
+    }
+}
