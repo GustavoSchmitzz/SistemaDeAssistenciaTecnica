b703490acb493ecda9b327b49dc0e5d751a51916
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-07-29 01:36:54 -0400

    feat: cria o ClienteController.

---
+++ src/main/java/com/assistencia/controller/ClienteController.java
@@ -0,0 +1,103 @@
+package com.assistencia.controller;
+
+import com.assistencia.dto.ClienteCadastroRequestDTO;
+import com.assistencia.entity.Cliente;
+import com.assistencia.service.ClienteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ClienteController  implements HttpHandler {
+    private final ClienteService clienteService;
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestMethod = exchange.getRequestMethod();
+        String requestPath = exchange.getRequestURI().getPath();
+        Integer id = extrairIdDaURL(requestPath);
+        try {
+            switch (requestMethod) {
+                case "POST":
+                    processarCadastroCliente(exchange);
+                    break;
+                case "PUT":
+                    if (id == null) throw new IllegalArgumentException("id nao informado.");
+                    processarAtualizacao(exchange, id);
+                    break;
+                case "GET":
+                    if (id == null) throw new IllegalArgumentException("id nao informado.");
+                    processaBuscaPorId(exchange, id);
+                    break;
+                default:
+                    enviarResposta(exchange, 405, "{\"erro\": \"metodo nao permitido\"}");
+            }
+        }catch(IllegalArgumentException e) {
+            enviarResposta(exchange, 400, "{\"erro\": \"" + e.getMessage()+ "\"}");
+        }catch (Exception e) {
+            enviarResposta(exchange, 500, "{\"erro\": \"erro do servidor\"}");
+        }
+    }
+
+    private void processarCadastroCliente(HttpExchange exchange) throws IOException {
+        InputStream requestBody = exchange.getRequestBody();
+        ClienteCadastroRequestDTO dto = new ObjectMapper().readValue(requestBody,
+                ClienteCadastroRequestDTO.class);
+
+        Cliente cliente = new Cliente();
+        cliente.setNome(dto.nome());
+        cliente.setEmail(dto.email());
+        cliente.setTelefone(dto.telefone());
+
+        Cliente salvo = clienteService.cadastrar(cliente);
+
+        String json = new ObjectMapper().writeValueAsString(salvo);
+        enviarResposta(exchange, 201, json);
+    }
+    private void processarAtualizacao(HttpExchange exchange, int id) throws IOException {
+        InputStream requestBody = exchange.getRequestBody();
+        ClienteCadastroRequestDTO dto = new ObjectMapper().readValue(requestBody,
+                ClienteCadastroRequestDTO.class);
+
+        Cliente cliente = new Cliente();
+        cliente.setNome(dto.nome());
+        cliente.setEmail(dto.email());
+        cliente.setTelefone(dto.telefone());
+
+        boolean salvo = clienteService.atualizar(cliente);
+        if (salvo) {
+            enviarResposta(exchange, 200, "{\"mensagem\":\"Cliente atualizado com sucesso\"}");
+        } else {
+            enviarResposta(exchange,404, "{\"erro\": \"Falha. Cliente nao encontrado.\" }");
+        }
+    }
+    private void processaBuscaPorId(HttpExchange exchange, int id) throws IOException {
+        Cliente cliente = clienteService.buscaPorId(id);
+        String json = new ObjectMapper().writeValueAsString(cliente);
+        enviarResposta(exchange, 200, json);
+    }
+    private void enviarResposta(HttpExchange exchange, int codigoHTTP, String respostaJson) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "application/json; charset=utf-8");
+        exchange.sendResponseHeaders(codigoHTTP, respostaJson.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(respostaJson.getBytes());
+        os.close();
+    }
+    private Integer extrairIdDaURL(String path) {
+        String[] caminho = path.split("/");
+        if (caminho.length > 2) {
+            try {
+                return Integer.parseInt(caminho[2]);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
