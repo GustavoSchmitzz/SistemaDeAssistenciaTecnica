659513d1742dca3209956e15372fd4eeb839f814
Gustavo Acassio Elias Schmitz <gustavoacassioj25@gmail.com>
2026-08-03 21:29:16 -0400

    feat: cria o controller responsavel pela entidade OrdemDeServico.

--- src/main/java/com/assistencia/service/OrdemDeServicoService.java
+++ src/main/java/com/assistencia/service/OrdemDeServicoService.java
@@ -2,21 +2,20 @@ package com.assistencia.service;
 
 import com.assistencia.entity.OrdemDeServico;
 import com.assistencia.repository.FornecedorRepository;
+import com.assistencia.repository.OrdemDeServicoRepository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class OrdemDeServicoService {
-    private final FornecedorRepository fornecedorRepository;
-    public OrdemDeServicoService(FornecedorRepository fornecedorRepository) {
-        this.fornecedorRepository = fornecedorRepository;
+    private final OrdemDeServicoRepository ordemDeServicoRepository;
+    public OrdemDeServicoService(OrdemDeServicoRepository ordemDeServicoRepository) {
+        this.ordemDeServicoRepository = ordemDeServicoRepository;
     }
 
     public OrdemDeServico abrirOrdem(OrdemDeServico ordemDeServico) {
         if (ordemDeServico == null) {
             throw new IllegalArgumentException("ordemDeServico nao pode ser nulo.");
-        }
-        if (ordemDeServico.getDataInicio() == null) {
-            throw new IllegalArgumentException("dataInicio nao pode ser nulo.");
         }
         if (ordemDeServico.getValorServico() < 0) {
             throw new IllegalArgumentException("valorServico nao pode ser menor que zero.");
@@ -34,8 +33,48 @@ public class OrdemDeServicoService {
         if (ordemDeServico.getStatusServico() == null)  {
             throw new IllegalArgumentException("StatusServico nao pode ser nulo.");
         }
+        if (ordemDeServico.getGarantia() == null) {
+            throw new IllegalArgumentException("Garantia nao pode ser nulo.");
+        }
+
+        ordemDeServico.setDataInicio(LocalDate.now());
+
+        ordemDeServicoRepository.cria(ordemDeServico);
 
         return ordemDeServico;
     }
+    public OrdemDeServico buscaOId(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("id nao pode ser igual ou menor que zero");
+        }
+        OrdemDeServico os = ordemDeServicoRepository.buscaOID(id);
+        if (os == null) {
+            throw new IllegalArgumentException("OrdemDeServico nao encontrada.");
+        }else  {
+            return os;
+        }
+    }
+    public boolean atualiza(OrdemDeServico ordemDeServico) {
+        if (ordemDeServico == null) {
+            throw new IllegalArgumentException("ordemDeServico nao pode ser nulo.");
+        }
+        double valorServico = ordemDeServico.getValorServico();
+        if(BigDecimal.valueOf(valorServico).scale() > 2 || valorServico < 0) {
+            throw new IllegalArgumentException("O valor do servico nao deve ter mais de duas casas decimais e menor que 0.");
+        }
+        if(ordemDeServico.getFuncionario() == null) {
+            throw new IllegalArgumentException("Tecnico nao pode ser nulo.");
+        }
+        if (ordemDeServico.getPeca() == null) {
+            throw new IllegalArgumentException("Peca nao pode ser nulo.");
+        }
+        if (ordemDeServico.getStatusServico() == null)  {
+            throw new IllegalArgumentException("StatusServico nao pode ser nulo.");
+        }
+        if (ordemDeServico.getGarantia() == null) {
+            throw new IllegalArgumentException("Garantia nao pode ser nulo.");
+        }
 
+        return ordemDeServicoRepository.atualiza(ordemDeServico);
+    }
 }
